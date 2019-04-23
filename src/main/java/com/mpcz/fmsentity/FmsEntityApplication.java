a31c603a3ab18b63e3719bfd844dc24a22259c75
hemantmpcz <hemant.mpcz@gmail.com>
2019-04-23 11:31:54 +0530

    First Commit

---
+++ src/main/java/com/mpcz/fmsentity/FmsEntityApplication.java
@@ -0,0 +1,13 @@
+package com.mpcz.fmsentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmsEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmsEntityApplication.class, args);
+	}
+
+}
